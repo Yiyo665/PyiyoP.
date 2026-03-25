6687a5299385e51a380144944a8e3cbe1f73b5a7
Yiyo665 <126626980+Yiyo665@users.noreply.github.com>
2026-03-25 20:10:06 -0300

    Initialize Kivy app for Parking PRO

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Parking PRO Android listo')
+from kivy.app import App
+from kivy.uix.label import Label
+
+class ParkingApp(App):
+    def build(self):
+        return Label(text="Parking PRO funcionando")
+
+ParkingApp().run()
